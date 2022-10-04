94488bdce7f35f152e0f2bdb84058f826cb50290
Jenny Gardner <j.rongjie@gmail.com>
2022-10-04 08:37:40 -0400

    update security

--- udemy-intro-django/todo-app/myList/settings.py
+++ udemy-intro-django/todo-app/myList/settings.py
@@ -20,7 +20,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-g!k=v7+bxrvbmuwq*n@_wbi-bu=*x431*0kxjmm*^)-^61a+h)'
+from decouple import config
+
+SECRET_KEY = config("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
